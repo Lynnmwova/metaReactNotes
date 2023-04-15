98d2e4173122d632689127e69b7dc4e175fdebcc
Lynnmwova <82355571+Lynnmwova@users.noreply.github.com>
2023-04-15 22:42:09 +0300

    file name extension changes and imports to app

--- exercises/metaReactExamples/src/components/Intro2.jsx
+++ exercises/metaReactExamples/src/components/Intro2.jsx
@@ -1,7 +1,7 @@
 function Intro2(){
     return(
 <div className="blog-post-intro">
-    <h2> Why I love frontend devlopment</h2>
+    <h2> Why I love frontend development</h2>
     <div>
         <p> In this blogpost, I'll list 10 reasons why I love working as a frontend developer</p>
         <p className="link">Read more..</p>
